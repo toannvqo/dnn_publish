bad7862e0f231623dd0df9b12b2473a1dbe9005b
toannvqo <toannvqo@gmail.com>
2016-05-10 11:05:57 +0700

    add heloloe

--- Components/ProductController.cs
+++ Components/ProductController.cs
@@ -13,7 +13,7 @@ namespace Christoc.Modules.DNNModule1.Components
             Product p;
             using (IDataContext ctx = DataContext.Instance())
             {
-                Console.WriteLine("hello");
+                Console.WriteLine("hello world");
                 var rep = ctx.GetRepository<Product>();
                 p = rep.GetById(productId);
             }
